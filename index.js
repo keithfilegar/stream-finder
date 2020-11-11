e251398c7af777d1c40712c243d0152c9a03d054
keithfilegar <keith.filegar@gmail.com>
2020-11-11 10:59:37 -0800

    set up event listener for search button

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const apiKey = 'bddc714743msh7cb71e3d76c6f90p121999jsnb6ff9b4a7f9f'
+
 const store = {
     searchStarted: false,
     searchList: [],
     viewPageKey: ""
 }
 
+// ======== HTML GENERATION ==========
+
 function generateHomePage() {
     return `
     <h1>Stream Finder</h1>
     <h3>Too many streaming services to keep track of? Search below to find out where to watch something!</h3>
-    <form class="js-user-form">
+    <form class="searchContent">
         <label for="searchSubject">Search for a TV show or Movie!</label>
         <br>
         <input type="text" id="searchSubject" required>
@@ -17,8 +21,38 @@ function generateHomePage() {
     <div class="js-error-message hidden"></div>`
 }
 
+// ======== API INTERACTIONS ==========
+
+function getUserSearch(searchTerm) {
+
+}
+
+// ======== EVENT HANDLERS ==========
+
+function handleUserSearch() {
+    $('.js-content-container').on('click', '.js-submit', event => {
+        event.preventDefault();
+        console.log("Working");
+        const searchTerm = $('#searchSubject').val();
+
+    })
+}
+
+// ======== RENDER ==========
+
+function renderHomeView() {
+    $('.js-content-container').html(generateHomePage());
+}
+
 function render() {
     if(store.searchStarted === false) {
         renderHomeView();
     }
 }
+
+function handleApp(){
+    render();
+    handleUserSearch();
+}
+
+$(handleApp);
