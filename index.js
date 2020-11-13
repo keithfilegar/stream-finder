ac4bb644ebe20e3c2b5ebc2bca3b231c34bc94a5
keithfilegar <keith.filegar@gmail.com>
2020-11-13 14:08:09 -0800

    begin setting up detail view

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const baseURL = 'https://imdb8.p.rapidapi.com/title'
 
 const store = {
     searchStarted: false,
-    detailId: ""
+    detailId: "",
+    listId: ""
 }
 
 const options = {
@@ -29,7 +30,7 @@ function generateHomePage() {
     <div class="js-error-message hidden"></div>`
 }
 
-function generateResultsHeader() {
+function generateResultsHeader(responseJson) {
     return `
     <header class="group">
         <h1 class="item">Stream Finder</h1>
@@ -41,9 +42,36 @@ function generateResultsHeader() {
         </form>
     </header>
 
+    <h2>Showing Resuls for: "${responseJson.query}"</h2>
+
     <div>
         <ul class="js-list-container"></ul>
     </div>`
+}
+
+function generateListPage(responseJson) {
+    //button id is structured to format id value needed to make additional calls
+    return `
+    <li class="group">
+        <div class="list-info item">
+            <h3>${responseJson.results[i].title}</h3>
+            <p>${responseJson.results[i].year}</p>
+            
+        </div>
+
+        <div class="list-image item">
+            <img src="${responseJson.results[i].image.url}">
+        </div>
+
+        <section id="listItem${i}" class="js-detail-container">
+            <button id="${responseJson.results[i].id.replace("/title/", "").replaceAll("/", "")}" class="list-button">Streaming Details</button>
+        </section>
+    </li>
+    `
+}
+
+function generateDetailView(responseJson) {
+    
 }
 
 // ======== API INTERACTIONS ==========
@@ -58,21 +86,20 @@ function formatSearchQuery(params) {
 
 function displaySearchResults(responseJson) {
     console.log(responseJson)
-    $('.js-content-container').empty();
-
-
+    
     // Display error message if search yields no results
     if(!responseJson.hasOwnProperty('results')){
         console.log('no results found')
-        $('.js-content-container').append(
-            `<div class="no-results-error">
-                <h3>Sorry! We weren't able to find anything by that name. Please try another search.</h3>
-            </div>`
+
+        $('.js-error-message').removeClass('hidden')
+        $('.js-error-message').append(
+            `<h3>Sorry! We weren't able to find anything by that name. Please try another search.</h3>`
         )
         return
     }
+    $('.js-content-container').empty();
 
-    $('body').html(generateResultsHeader());
+    $('body').html(generateResultsHeader(responseJson));
 
     for(i = 0; i < responseJson.results.length; i++) {
         //filter out unwanted response values
@@ -80,19 +107,7 @@ function displaySearchResults(responseJson) {
             continue;
         }
 
-        //button id is structured to format id value needed to make additional calls
-        $('.js-list-container').append(
-            `<li class="group">
-                <div class="item">
-                <h3>${responseJson.results[i].title}</h3>
-                <p>${responseJson.results[i].year}</p>
-                <button id="${responseJson.results[i].id.replace("/title/", "").replaceAll("/", "")}" class="list-button">Streaming Details</button>
-                </div>
-
-                <div class="item">
-                <img src="${responseJson.results[i].image.url}">
-            </li>`
-        )
+        $('.js-list-container').append(generateListPage(responseJson))
     }
 
 }
@@ -116,12 +131,20 @@ function getUserSearch(searchTerm) {
     })
     .then(responseJson => displaySearchResults(responseJson))
     .catch(error => {
-        alert("Something went wrong. Please try again later.")
-        console.log(error)
+        // TODO: Identify error when searching word "rubber"
+        //alert("Something went wrong. Please try again later.")
+        console.log(error.status)
     })
 }
 
 // --------- Handle Detail View GET title/overview-detail ----------
+function displayOverviewResults(responseJson){
+    console.log(responseJson)
+    $('#' + store.listId).empty();
+
+    $('#' + store.listId).apend(generateDetailView(responseJson));
+    
+}
 
 function getOverviewDetails() {
     const params = {
@@ -140,10 +163,10 @@ function getOverviewDetails() {
         }
         return response.json()
     })
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayOverviewResults(responseJson))
     .catch(error => {
-        alert("Something went wrong. Please try again later.")
-        console.log(error)
+        //alert("Something went wrong. Please try again later.")
+        console.log(error.status)
     })
 }
 
@@ -161,10 +184,13 @@ function handleUserSearch() {
 }
 
 function handleStreamDetails() {
-    $('body').on('click', '.list-button', event => {
+    $('body').on('click', '.js-detail-container', event => {
         store.detailId = event.target.id
         console.log(store.detailId)
 
+        store.listId = event.currentTarget.id
+        console.log(store.detailId)
+        console.log(store.listId)
         getOverviewDetails();
     })
 }
